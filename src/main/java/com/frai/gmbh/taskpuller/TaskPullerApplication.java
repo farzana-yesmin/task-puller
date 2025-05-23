1fb7403ab5a7f84924ee4639312cb27ad08deec7
farzana-yesmin <farzana_cse15@yahoo.com>
2025-05-23 22:59:05 +0600

    chore: initialize project with Web and Lombok dependencies

---
+++ src/main/java/com/frai/gmbh/taskpuller/TaskPullerApplication.java
@@ -0,0 +1,13 @@
+package com.frai.gmbh.taskpuller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPullerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskPullerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frai/gmbh/taskpuller/TaskPullerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frai.gmbh.taskpuller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPullerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
